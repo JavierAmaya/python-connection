52a6f65bc17a341b5daa05200df4d9d1b83dbc31
JavierAmaya <photojavhn@gmail.com>
2020-04-09 17:03:49 -0600

    finally commit

--- models/test.py
+++ models/test.py
@@ -1,6 +1,6 @@
 import pyodbc
 
-conn = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server=tcp:unahvjamaya.database.windows.net,1433;Database=ingreso_vehiculos;Uid=vjamaya;Pwd=Admin203Amaya.;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+conn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:myservername.database.windows.net,1433;Database=mydatabase;Uid=myuser;Pwd=mypasword.;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
 
 cursor = conn.cursor()
 cursor.execute('SELECT * FROM myExample.Products')
